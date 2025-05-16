32822527b23eb96440f810bde8c28de1d664606d
gnich12 <jinto12@gmail.com>
2025-05-16 16:31:50 -0700

    init
    init

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static("public"));
+app.use("/assets", express.static("assets"));
+
+io.on("connection", (socket) => {
+    console.log("A user connected");
+
+    socket.on("tap", (data) => {
+        console.log(data)
+        io.emit("displayTap", data); // Broadcast event to all connected clients
+    });
+
+    socket.on("disconnect", () => {
+        console.log("A user disconnected");
+    });
+});
+
+server.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
